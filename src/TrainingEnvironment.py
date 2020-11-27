4894093a8c079ea910f1b9324544c0799ff98baa
Håvard <haavard.godal@gmail.com>
2020-11-27 09:19:28 +0100

    Added sanity check for repeating steps (#8)

--- src/TrainingEnvironment.py
+++ src/TrainingEnvironment.py
@@ -5,8 +5,8 @@ import numpy as np
 import functions
 
 population_size = 50
-generations = 1
-mutation_rate = 20
+generations = 1  # 15
+mutation_rate = 0.001
 
 env = gym.make('CartPole-v1')
 
@@ -16,11 +16,16 @@ for i in range(generations):
     for n, agent in enumerate(population):
         observation = env.reset()
         score = 0
-        for j in range(1000):
-            observation, reward, done, info = env.step(
-                int(agent.predict(observation.reshape(1, -1).reshape(1, -1)))
-            )
+        actions = np.empty(5)
+        for j in range(500):
+            action = int(agent.predict(
+                observation.reshape(1, -1).reshape(1, -1)))
+            if j >= 5 and sum(actions) % 5 == 0:
+                action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
             score += reward
+            actions[j % 5] = action
+            # env.render()
             if done:
                 break
 
