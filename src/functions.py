b5e5a21de9c137663efa4086ef4ac2ebc19d2a98
Håvard <8015540+HGodal@users.noreply.github.com>
2020-12-04 23:05:59 +0100

    Restructured to better match functions currently in use

--- src/functions.py
+++ src/functions.py
@@ -4,35 +4,43 @@ Python helper functions for training agents.
 Functions:
     create_new_network - Create new MLP Classifier.
     initialise_population -  Initalises a population of agents.
-    mutationFunc_W_B -  Mutate both weights and biases for an agent.
+    simulate_agent - Simulates the chosen agent in the environment.
     de_crossover - Differential crossover.
-    breedCrossover - Breeds a child from 2 parents using crossover.
-    show_simulation - Display a simulation of a single given network.
+    mutationFunc_W_B -  Mutate both weights and biases for an agent.
     average_weight_and_bias - Calculate the average weight and bias.
-    partial_fit - Function for partial fitting model on experience.
     nnPerformance - Visualize the performance from each generation.
+    save_frames_as_gif - Storing gif of trained agent.
+
+    partial_fit - Partial fitting model on experience.
+    breedCrossover - Breeds a child using two point crossover.
+    de_crossover_classic - Breeds a child from a classic de algorithm.
+    crossoverSinglePoint - Breeds two children using one point crossover.
     mutation_rate - Dynamic mutation rate function.
-    save_frames_as_gif - Function for storing gif of trained agent.
+    
 
 Functions by Author:
     Bjørn Christian Weinbach:
         create_new_network
         initialise_population
+        simulate_agent
+        save_frames_as_gif
         partial_fit
         mutation_rate
-        save_frames_as_gif
 
     Marius Sørensen:
-        show_simulation
         create_new_network
+        show_simulation
+        simulate_agent
 
     Ove Jørgensen:
         mutationFunc_W_B
         average_weight_and_bias
 
     Håvard Godal:
+        simulate_agent
         de_crossover
         breedCrossover
+        de_crossover_classic
 
     Johanna Kinstad:
         mutationFunc_W_B
@@ -41,6 +49,7 @@ Functions by Author:
         mutationFunc_W_B
         nnPerformance
         crossoverSinglePoint
+        mutationFunc
 """
 
 from sklearn.neural_network import MLPClassifier
@@ -92,6 +101,88 @@ def initialise_population(size, env):
     for _ in range(size):
         population.append(create_new_network(env))
     return population
+
+
+def simulate_agent(agent, env, render=False, savetofile=False, filename=None):
+    """Simulate an agent in an environment and return its fitness score.
+
+    Author: [Håvard Godal, Marius Sørensen, Bjørn Christian Weinbach]
+
+    Args:
+        agent (MLPClassifier): [Neural Network]
+        env ([OpenAI Gym]): [Environment]
+
+    Returns:
+        [score]: [Fitness score of agent]
+    """
+    observation = env.reset()
+    score = 0
+    actions = np.empty(5)
+    done = False
+
+    frames = []
+
+    # Agent-Environment Interaction
+    while not done:
+        action = int(agent.predict(
+            observation.reshape(1, -1)))
+        observation, reward, done, _ = env.step(action)
+        score += reward
+
+        if savetofile:
+            frames.append(env.render(mode="rgb_array"))
+        if render:
+            env.render()
+    if savetofile:
+        save_frames_as_gif(frames, path='./gifs/', filename=filename)
+
+    return score
+
+
+def de_crossover(nn1, nn2):
+    """Create new weights and biases through a modified differential evolution crossover.
+
+    Author: Håvard Godal
+
+    Args:
+        nn1 (MLPClassifier): [Neural Network]
+        nn2 (MLPClassifier): [Neural Network]
+
+    Returns:
+        [newcoeffs]: [Weights of new network]
+        [newintercepts]: [Biases of new network]
+    """
+    newcoefs = []
+    newintercepts = []
+    for i in range(2):
+        shape = nn1.coefs_[i].shape
+        coef1Flat = np.ravel(nn1.coefs_[i])
+        coef2Flat = np.ravel(nn2.coefs_[i])
+
+        newcoefs.append(
+            np.array(
+                coef1Flat + np.random.uniform(
+                    0,
+                    1,
+                    len(coef1Flat)
+                ) * (coef2Flat-coef1Flat)
+            ).reshape(shape)
+        )
+
+        shape = nn1.intercepts_[i].shape
+        intercepts1Flat = np.ravel(nn1.intercepts_[i])
+        intercepts2Flat = np.ravel(nn2.intercepts_[i])
+
+        newintercepts.append(
+            np.array(
+                intercepts1Flat + np.random.uniform(
+                    0,
+                    1,
+                    len(intercepts1Flat)
+                ) * (intercepts2Flat-intercepts1Flat)
+            ).reshape(shape)
+        )
+    return newcoefs, newintercepts
 
 
 def mutationFunc_W_B(agent, mutation_rate, method):
@@ -146,6 +237,191 @@ def mutationFunc_W_B(agent, mutation_rate, method):
                                     inner = random.random()
 
     return agent
+
+
+def average_weight_and_bias(population, env):
+    """Calculate the average weight and bias from a given population.
+
+    Author: Ove Jørgensen
+
+    Args:
+        population: The population from which to calculate
+        env: OpenAI Gym environment.
+
+    Returns:
+        [avg_network]: A new network given the average bias and weight
+    """
+    def find_mean(mat, attr_type, i): return np.mean(
+        np.array([getattr(el, attr_type)[i] for el in mat]),
+        axis=0
+    )
+    coef0 = find_mean(population, 'coefs_', 0)
+    coef1 = find_mean(population, 'coefs_', 1)
+    intercept0 = find_mean(population, 'intercepts_', 0)
+    intercept1 = find_mean(population, 'intercepts_', 1)
+
+    avg_network = create_new_network(env)
+    avg_network.coefs_ = [coef0, coef1]
+    avg_network.intercepts_ = [intercept0, intercept1]
+
+    return avg_network
+
+
+def nnPerformance(generation, best_score, average_score, acceptanceCriteria):
+    """Visualize the performance from each generation.
+
+    Author: Vegard Rongve
+
+    Args:
+        generation: generation size
+        best_score: list with best scores from each generation
+        average_score: list with average scores from each generation
+        acceptanceCriteria: Horisontal line of acceptance ratio.
+
+    Returns:Plot
+    """
+    plt.plot(range(generation), best_score, label="Max score")
+    plt.plot(range(generation), average_score, label="Average score")
+    plt.title('Fitness through the generations')
+    plt.axhline(y=acceptanceCriteria, color='r',
+                linestyle='--', label="Acceptance ratio")
+    plt.legend()
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness")
+    plt.show()
+
+
+def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
+    """Save environment render as gif.
+
+    "Stolen" from github user botforge. Modified to make it compatible.
+    url: https://gist.github.com/botforge/64cbb71780e6208172bbf03cd9293553
+
+    Author: Github: botforge, Modified Bjørn Christian Weinbach
+
+    Args:
+        frames (List): [List of env.render(mode="rgb_array")]
+        path (str, optional): [Filepath]. Defaults to './'.
+        filename (str, optional): [Filename of gif].
+    """
+    # Mess with this to change frame size
+    plt.figure(figsize=(frames[0].shape[1] / 72.0,
+                        frames[0].shape[0] / 72.0), dpi=72)
+
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(
+        plt.gcf(), animate, frames=len(frames), interval=50)
+
+    anim.save(path + filename, writer='pillow', fps=60)
+
+
+########################
+# Deprecated functions #
+########################
+
+
+def partial_fit(best_trained, best_network, env):
+    """Partial fit neural netowork to actions of best agent.
+
+    Author: Bjørn Christian Weinbach
+
+    Args:
+        best_trained (MLPClassifier): [Neural network to be trained]
+        best_network (MLPClassifier): [Neural network that scored high]
+        env (OpenAI gym): [Environment]
+
+    Returns:
+        [best_trained]: [partially fitted neural netork]
+    """
+    observation = env.reset()
+    score = 0
+    actions = np.empty(5)
+    trainingx = []
+    trainingy = []
+    actions
+    terminate = False
+
+    while not(terminate):
+        j = 0
+        action = int(best_network.predict(
+            observation.reshape(1, -1).reshape(1, -1)))
+        if j > 5 and sum(actions) % 5 == 0:
+            action = env.action_space.sample()
+        observation, reward, done, _ = env.step(action)
+        trainingx.append(observation)
+        trainingy.append(action)
+        score += reward
+        j += 1
+        actions[j % 5] = action
+        terminate = done
+
+    best_trained.partial_fit(
+        trainingx,
+        trainingy
+    )
+
+    return best_trained
+
+
+def breedCrossover(nn1, nn2):
+    """Breeds a child from 2 parents using crossover.
+
+    Author: Håvard Godal
+
+    Args:
+        nn1 ([MLPClassifier]): [Neural network parent nr 1]
+        nn2 ([MLPClassifier]): [Neural network parent nr 2]
+
+    Returns:
+        [children]: [List of two children containing coefs_ and intercepts_]
+    """
+    # Choosing either input -> hidden-layer or hidden-layer -> output
+    layer = random.randint(0, 1)
+    # layer = 1
+
+    child1 = []
+    child2 = []
+
+    for i in range(2):
+        if i == 0:
+            param1 = nn1.coefs_
+            param2 = nn2.coefs_
+        else:
+            param1 = nn1.intercepts_
+            param2 = nn2.intercepts_
+
+        shape = param2[layer].shape
+
+        paramFlat1 = np.ravel(param1[layer])
+        paramFlat2 = np.ravel(param2[layer])
+
+        indexes = sorted([int(random.random() * len(paramFlat1)),
+                          int(random.random() * len(paramFlat1))])
+
+        newFlatParam = copy.copy(paramFlat2)
+        newFlatParam[indexes[0]:indexes[1]] = paramFlat1[indexes[0]:indexes[1]]
+
+        newParam = []
+        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
+        newParam.insert(1 - layer, param2[1 - layer])
+
+        child1.append(newParam)
+
+        newFlatParam = copy.copy(paramFlat1)
+        newFlatParam[indexes[0]:indexes[1]] = paramFlat2[indexes[0]:indexes[1]]
+
+        newParam = []
+        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
+        newParam.insert(1 - layer, param1[1 - layer])
+
+        child2.append(newParam)
+
+    return [child1, child2]
 
 
 def de_crossover_classic(agent, randomAgents):
@@ -206,241 +482,8 @@ def de_crossover_classic(agent, randomAgents):
     return newcoefs, newintercepts
 
 
-def de_crossover(nn1, nn2):
-    """Create new weights and biases through a modified differential evolution crossover.
-
-    Author: Håvard Godal
-
-    Args:
-        nn1 (MLPClassifier): [Neural Network]
-        nn2 (MLPClassifier): [Neural Network]
-
-    Returns:
-        [newcoeffs]: [Weights of new network]
-        [newintercepts]: [Biases of new network]
-    """
-    newcoefs = []
-    newintercepts = []
-    for i in range(2):
-        shape = nn1.coefs_[i].shape
-        coef1Flat = np.ravel(nn1.coefs_[i])
-        coef2Flat = np.ravel(nn2.coefs_[i])
-
-        newcoefs.append(
-            np.array(
-                coef1Flat + np.random.uniform(
-                    0,
-                    1,
-                    len(coef1Flat)
-                ) * (coef2Flat-coef1Flat)
-            ).reshape(shape)
-        )
-
-        shape = nn1.intercepts_[i].shape
-        intercepts1Flat = np.ravel(nn1.intercepts_[i])
-        intercepts2Flat = np.ravel(nn2.intercepts_[i])
-
-        newintercepts.append(
-            np.array(
-                intercepts1Flat + np.random.uniform(
-                    0,
-                    1,
-                    len(intercepts1Flat)
-                ) * (intercepts2Flat-intercepts1Flat)
-            ).reshape(shape)
-        )
-    return newcoefs, newintercepts
-
-
-def breedCrossover(nn1, nn2):
-    """Breeds a child from 2 parents using crossover.
-
-    Author: Håvard Godal
-
-    Args:
-        nn1 ([MLPClassifier]): [Neural network parent nr 1]
-        nn2 ([MLPClassifier]): [Neural network parent nr 2]
-
-    Returns:
-        [children]: [List of two children containing coefs_ and intercepts_]
-    """
-    # Choosing either input -> hidden-layer or hidden-layer -> output
-    layer = random.randint(0, 1)
-    # layer = 1
-
-    child1 = []
-    child2 = []
-
-    for i in range(2):
-        if i == 0:
-            param1 = nn1.coefs_
-            param2 = nn2.coefs_
-        else:
-            param1 = nn1.intercepts_
-            param2 = nn2.intercepts_
-
-        shape = param2[layer].shape
-
-        paramFlat1 = np.ravel(param1[layer])
-        paramFlat2 = np.ravel(param2[layer])
-
-        indexes = sorted([int(random.random() * len(paramFlat1)),
-                          int(random.random() * len(paramFlat1))])
-
-        newFlatParam = copy.copy(paramFlat2)
-        newFlatParam[indexes[0]:indexes[1]] = paramFlat1[indexes[0]:indexes[1]]
-
-        newParam = []
-        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
-        newParam.insert(1 - layer, param2[1 - layer])
-
-        child1.append(newParam)
-
-        newFlatParam = copy.copy(paramFlat1)
-        newFlatParam[indexes[0]:indexes[1]] = paramFlat2[indexes[0]:indexes[1]]
-
-        newParam = []
-        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
-        newParam.insert(1 - layer, param1[1 - layer])
-
-        child2.append(newParam)
-
-    return [child1, child2]
-
-
-def simulate_agent(agent, env, render=False, savetofile=False, filename=None):
-    """Simulate an agent in an environment and return its fitness score.
-
-    Author: [Håvard Godal, Marius Sørensen, Bjørn Christian Weinbach]
-
-    Args:
-        agent (MLPClassifier): [Neural Network]
-        env ([OpenAI Gym]): [Environment]
-
-    Returns:
-        [score]: [Fitness score of agent]
-    """
-    observation = env.reset()
-    score = 0
-    actions = np.empty(5)
-    done = False
-
-    frames = []
-
-    # Agent-Environment Interaction
-    while not done:
-        action = int(agent.predict(
-            observation.reshape(1, -1)))
-        observation, reward, done, _ = env.step(action)
-        score += reward
-
-        if savetofile:
-            frames.append(env.render(mode="rgb_array"))
-        if render:
-            env.render()
-    if savetofile:
-        save_frames_as_gif(frames, path='./gifs/', filename=filename)
-
-    return score
-
-
-def average_weight_and_bias(population, env):
-    """Calculate the average weight and bias from a given population.
-
-    Author: Ove Jørgensen
-
-    Args:
-        population: The population from which to calculate
-        env: OpenAI Gym environment.
-
-    Returns:
-        [avg_network]: A new network given the average bias and weight
-    """
-    def find_mean(mat, attr_type, i): return np.mean(
-        np.array([getattr(el, attr_type)[i] for el in mat]),
-        axis=0
-    )
-    coef0 = find_mean(population, 'coefs_', 0)
-    coef1 = find_mean(population, 'coefs_', 1)
-    intercept0 = find_mean(population, 'intercepts_', 0)
-    intercept1 = find_mean(population, 'intercepts_', 1)
-
-    avg_network = create_new_network(env)
-    avg_network.coefs_ = [coef0, coef1]
-    avg_network.intercepts_ = [intercept0, intercept1]
-
-    return avg_network
-
-
-def partial_fit(best_trained, best_network, env):
-    """Partial fit neural netowork to actions of best agent.
-
-    Author: Bjørn Christian Weinbach
-
-    Args:
-        best_trained (MLPClassifier): [Neural network to be trained]
-        best_network (MLPClassifier): [Neural network that scored high]
-        env (OpenAI gym): [Environment]
-
-    Returns:
-        [best_trained]: [partially fitted neural netork]
-    """
-    observation = env.reset()
-    score = 0
-    actions = np.empty(5)
-    trainingx = []
-    trainingy = []
-    actions
-    terminate = False
-
-    while not(terminate):
-        j = 0
-        action = int(best_network.predict(
-            observation.reshape(1, -1).reshape(1, -1)))
-        if j > 5 and sum(actions) % 5 == 0:
-            action = env.action_space.sample()
-        observation, reward, done, _ = env.step(action)
-        trainingx.append(observation)
-        trainingy.append(action)
-        score += reward
-        j += 1
-        actions[j % 5] = action
-        terminate = done
-
-    best_trained.partial_fit(
-        trainingx,
-        trainingy
-    )
-
-    return best_trained
-
-
-def nnPerformance(generation, best_score, average_score, acceptanceCriteria):
-    """Visualize the performance from each generation.
-
-    Author: Vegard Rongve
-
-    Args:
-        generation: generation size
-        best_score: list with best scores from each generation
-        average_score: list with average scores from each generation
-        acceptanceCriteria: Horisontal line of acceptance ratio.
-
-    Returns:Plot
-    """
-    plt.plot(range(generation), best_score, label="Max score")
-    plt.plot(range(generation), average_score, label="Average score")
-    plt.title('Fitness through the generations')
-    plt.axhline(y=acceptanceCriteria, color='r',
-                linestyle='--', label="Acceptance ratio")
-    plt.legend()
-    plt.xlabel("Generations")
-    plt.ylabel("Fitness")
-    plt.show()
-
-
 def crossoverSinglePoint(parent1, parent2):
-    """Breeds two childs from 2 parents using single point crossover.
+    """Breeds two children from 2 parents using single point crossover.
 
     Author: Vegard Rongve
 
@@ -458,11 +501,9 @@ def crossoverSinglePoint(parent1, parent2):
         if item == 0:
             p1 = parent1.coefs_
             p2 = parent2.coefs_
-            # print("Test1")
         else:
             p1 = parent1.intercepts_
             p2 = parent2.intercepts_
-            # print("Test2")
 
         for i in range(2):
             if i == 0:
@@ -476,9 +517,6 @@ def crossoverSinglePoint(parent1, parent2):
             flatParam1 = np.ravel(param1)
             flatParam2 = np.ravel(param2)
 
-            # randIndex = int(random.random()*flatParam1.size)
-            randIndex = 1
-
             ch1 = np.concatenate(
                 (flatParam1[:randIndex], flatParam2[randIndex:]))
             ch2 = np.concatenate(
@@ -491,7 +529,7 @@ def crossoverSinglePoint(parent1, parent2):
             child1.append(ch1Reshape)
             child2.append(ch2Reshape)
 
-    return [child1[:2], child1[2:]], [child2[:2], child2[2:]], randIndex
+    return [child1[:2], child1[2:]], [child2[:2], child2[2:]], 1
 
 
 def mutationFunc(agent, mutation_rate):
@@ -548,32 +586,3 @@ def mutation_rate(score, goal):
         (float): [Mutation rate]
     """
     return(1 - (score/goal))
-
-
-def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
-    """Save environment render as gif.
-
-    "Stolen" from github user botforge. Modified to make it compatible.
-    url: https://gist.github.com/botforge/64cbb71780e6208172bbf03cd9293553
-
-    Author: Github: botforge, Modified Bjørn Christian Weinbach
-
-    Args:
-        frames (List): [List of env.render(mode="rgb_array")]
-        path (str, optional): [Filepath]. Defaults to './'.
-        filename (str, optional): [Filename of gif].
-    """
-    # Mess with this to change frame size
-    plt.figure(figsize=(frames[0].shape[1] / 72.0,
-                        frames[0].shape[0] / 72.0), dpi=72)
-
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-
-    def animate(i):
-        patch.set_data(frames[i])
-
-    anim = animation.FuncAnimation(
-        plt.gcf(), animate, frames=len(frames), interval=50)
-
-    anim.save(path + filename, writer='pillow', fps=60)
